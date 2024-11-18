17314f760a758cedf74e92c306547cf72fe26071
Tushar Pal <Tusharpal353@gmail.com>
2024-11-18 22:21:42 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import Header from "./Components/Header/Header";
+import Body from "./Components/Body/Body"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+
+
+    <div>
+      <Header/>
+      
+      
     </div>
+
+
+
+
+
+   
+      {/*
+     1 #Header
+        hamburger menu
+        logo
+        searchbar
+        profile
+     2 #Body
+        -sidebar
+          menuitems
+        -mainbody
+          Button list
+          video container 
+          video card
+      */}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar from '../Sidebar/Sidebar'
+
+const Body = () => {
+  return (
+    <>
+    <Sidebar/>
+    </>
+  )
+}
+
+export default Body
