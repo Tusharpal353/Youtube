f9e756c469852cffa2abe3b9f414292ee0f27860
Tushar Pal <Tusharpal353@gmail.com>
2024-11-19 17:32:38 +0530

    Remove sensitive file

--- src/Utils/Constant.js
+++
@@ -1,3 +0,0 @@
-const GOOGLE_APIKEY = "AIzaSyB7kjm44NxZuLzjANZ9mBsKP8yVhDFH0Z0"
-export const VIDEO_URL=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=${GOOGLE_APIKEY}`
-/* &maxResults=50 */
