fea52808a4137328ebe9bf5b4836076138b06866
Tushar Pal <Tusharpal353@gmail.com>
2024-11-21 21:32:51 +0530

    made seach bar functionality

---
+++ Back/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require("cors")
+const app = express();
+app.use(cors())
+app.get('/', async (req, res) => {
+ /*  const query = req.query.q; */
+
+  const url = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=iphone";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    
+    res.json(data);
+  } catch (error) {
+    res.status(500).send('Error fetching suggestions');
+  }
+});
+
+app.listen(5000, () => console.log('Server running on port 5000'));
