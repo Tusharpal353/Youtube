03312ca1b5ecda3d56e459a675f48a16880366a0
Tushar Pal <Tusharpal353@gmail.com>
2024-11-29 17:42:46 +0530

    Implemented play video on search result

--- src/Components/SearchResult/SearchResultCard.jsx
+++ src/Components/SearchResult/SearchResultCard.jsx
@@ -1,27 +1,28 @@
-    import React from "react";
-
-    const SearchResultCard = ({item}) => {
+import React from "react";
 
 
-        
+const SearchResultCard = ({ item }) => {
+ 
 
-    return (
-        <>
-        <div className="flex mb-4 ">
-            <div>
-            <img className="rounded-xl shadow-xl" src={item.snippet.thumbnails.medium.url} alt="thumbnail" />
-            </div>
-            <div className="ml-2">
-            <h1 className="text-xl font-semibold">{item.snippet.title}</h1>
-            <h1>{item.snippet.publishedAt}</h1>
-            <h1>{item.snippet.channelTitle}</h1>
-            <h1 className="text-gray-500 text-xs">{item.snippet.description}</h1>
-            </div>
+  return (
+    <>
+      <div className="flex mb-4 " >
+        <div>
+          <img
+            className="rounded-xl shadow-xl"
+            src={item.snippet.thumbnails.medium.url}
+            alt="thumbnail"
+          />
         </div>
-        </>
-    );
-    };
+        <div className="ml-2">
+          <h1 className="text-xl font-semibold">{item.snippet.title}</h1>
+          <h1>{item.snippet.publishedAt}</h1>
+          <h1>{item.snippet.channelTitle}</h1>
+          <h1 className="text-gray-500 text-xs">{item.snippet.description}</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    export default SearchResultCard;
-
-   
+export default SearchResultCard;
--- src/Components/SearchResult/SearchReuslt.jsx
+++ src/Components/SearchResult/SearchReuslt.jsx
@@ -1,58 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { GOOGLE_APIKEY } from '../../Utils/Constant'
-import { useSearchParams } from "react-router-dom";
-import SearchResultCard from './SearchResultCard';
+import React, { useEffect, useState } from "react";
+import { GOOGLE_APIKEY } from "../../Utils/Constant";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import SearchResultCard from "./SearchResultCard";
 const SearchReuslt = () => {
   const [searchParams] = useSearchParams();
   const q = searchParams.get("q");
-  const [apiData,setApiData]=useState([])
+  const [apiData, setApiData] = useState([]);
 
+  const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const handleClick = () => {
+    if(id){
+    navigate(`/watch/?v=${id}`);
+  }};
 
-  const fetchVideos = ()=>{
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&maxResults=10&key=${GOOGLE_APIKEY}`)
-    .then(res => res.json())
-    .then(res => {
-      setApiData(res.items);
-      console.log(res.items, "search page"); // Log the fetched data here
-    });
-}
-useEffect(()=>{
+  const fetchVideos = () => {
+    let FetchData = fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${q}&type=video&maxResults=10&key=${GOOGLE_APIKEY}`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setApiData(res.items);
+        console.log(res.items, "search page"); // Log the fetched data here
+      });
+    
+     
+  };
+  useEffect(() => {
+    if (q) {
+      fetchVideos();
+    }
+  }, [q]);
 
-  if(q){
-    fetchVideos()
+  const handleVideoSelect = (videoId) => {
+    setId(videoId);
+    console.log("Selected Video ID:", videoId);
+  };
 
-  }
-},
-  [q] )
-  
   return (
     <div>
+      <div onClick={handleClick } className="cursor-pointer">
+        {apiData.length > 0 ? (
+          apiData.map((item, index) => (
+            <div onClick={() =>{ handleVideoSelect(item.id.videoId)
+              handleClick()
+            }}>
+              <SearchResultCard key={index} item={item} />
+            </div>
+          ))
+        ) : (
+          <p>loading </p>
+        )}
+      </div>
+    </div>    
+  );
+};
 
-<div>
-
-  {apiData.length>0?(
-
-    apiData.map((item,index)=><SearchResultCard key={index} item={item}/>)
-  ):(
-    <p>loading </p>
-
-  )
-  }
-      
-    </div>
-
-    </div>
-  )
-}
-
-export default SearchReuslt
-
-
-
-/* {apiData.length > 0 ? (
-        apiData.map((item, index) => (
-          <SearchResultCard key={index} item={item} />
-        ))
-      ) : (
-        <p>Loading...</p>
-      )} */
+export default SearchReuslt;
