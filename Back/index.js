690cbbb522efd0e552a4a84fd86dfd748d6f47d1
Tushar Pal <Tusharpal353@gmail.com>
2024-12-29 13:03:38 +0530

    backend fixes

--- Back/index.js
+++ Back/index.js
@@ -4,11 +4,16 @@ const app = express();
 app.use(cors())
 app.get('/', async (req, res) => {
   const query = req.query.q;
-
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter "q" is required' });
+}
   const url = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=${query}`;
 
   try {
     const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+  }
     const data = await response.json();
     console.log(data[1]);
     
